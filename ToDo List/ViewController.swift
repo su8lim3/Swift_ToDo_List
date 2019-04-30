defaa6303b3979b55f38c575eed49d63b187b274
Rico Alderete <su8lim3@gmail.com>
2019-04-29 21:05:18 -0600

    Initial Commit of the ToDo List App

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Rico Alderete on 4/29/19.
+//  Copyright © 2019 Rico Alderete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
