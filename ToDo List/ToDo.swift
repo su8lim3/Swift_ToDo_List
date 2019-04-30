1836585221447d0fd4b7df714dd950fdbe3cb218
Rico Alderete <su8lim3@gmail.com>
2019-04-30 08:07:23 -0600

    Adding to do list items to array and displaying them in list, also specifying important to do items

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Rico Alderete on 4/30/19.
+//  Copyright © 2019 Rico Alderete. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+}
