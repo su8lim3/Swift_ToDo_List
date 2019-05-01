e004f7e507b915ebd3c2c1e8e5b7e0f9a43aa07f
Rico Alderete <su8lim3@gmail.com>
2019-04-30 20:27:55 -0600

    Adding "add todo list item" view and class

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Rico Alderete on 4/30/19.
+//  Copyright © 2019 Rico Alderete. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+}
